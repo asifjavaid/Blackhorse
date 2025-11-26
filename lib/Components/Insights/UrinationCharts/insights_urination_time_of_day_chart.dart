39e3e12093ee2dea22aa8efdc27e2ce9aafa6d12
Asif Javaid <asifjavaidch@gmail.com>
2025-11-26 18:19:26 +0500

    fixed urination graph

--- lib/Components/Insights/UrinationCharts/insights_urination_time_of_day_chart.dart
+++ lib/Components/Insights/UrinationCharts/insights_urination_time_of_day_chart.dart
@@ -4,10 +4,14 @@ import 'package:ekvi/Utils/constants/app_colors.dart';
 import 'package:ekvi/Utils/constants/app_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
+import '../../../Core/themes/app_themes.dart';
 import '../../../Models/Insights/insights_graph_model.dart';
+import '../../../Providers/Insights/insights_symptom_time_selection_provider.dart';
+import '../../../Utils/helpers/helper_functions.dart';
 
 class InsightsTimeOfDayUrinationsChart extends StatefulWidget {
   final InsightsGraphModel data;
@@ -26,48 +30,78 @@ class _InsightsTimeOfDayUrinationsChartState
   final GlobalKey<SfCartesianChartState> chartKey =
       GlobalKey<SfCartesianChartState>();
 
+  String getHeadacheDurationLabel(TimeFrameSelection selectionType) {
+    switch (selectionType) {
+      case TimeFrameSelection.oneMonth:
+        return 'How many times you urinated per day';
+      case TimeFrameSelection.threeMonths:
+        return 'How many times you urinated per week';
+      case TimeFrameSelection.oneYear:
+        return 'How many times you urinated per month';
+      default:
+        return 'How many times you urinated per week';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
+    final provider = Provider.of<InsightsSymptomTimeSelectionProvider>(context);
 
     return Container(
       padding: const EdgeInsets.all(16.23),
       decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16.23),
-        ),
-      ),
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.23),
+          ),
+          shadows: const [AppThemes.shadowDown]),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Text('Urination frequency', style: textTheme.headlineSmall),
+          const SizedBox(height: 9),
+          Text(
+            getHeadacheDurationLabel(provider.selectionType),
+            style: textTheme.bodySmall!.copyWith(
+              color: AppColors.neutralColor500,
+              fontWeight: FontWeight.w400,
+              height: 1.60,
+            ),
+          ),
+          const SizedBox(height: 24),
+          ChartArea(
+            data: widget.data,
+          ),
+          const SizedBox(height: 24),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Urination frequency', style: textTheme.headlineSmall),
-                  ],
+              Text('Average frequency over 1 month',
+                  style: textTheme.labelSmall!
+                      .copyWith(color: AppColors.neutralColor500)),
+              const SizedBox(width: 5.07),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                decoration: ShapeDecoration(
+                  color: AppColors.errorColor400,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                child: Text(
+                  HelperFunctions.validateNumeric(widget.data.average),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: AppColors.neutralColor600,
+                    fontSize: 10,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
                 ),
               ),
-              GestureDetector(
-                onTap: widget.enableHelpCallback,
-                child: SvgPicture.asset(
-                  "${AppConstant.assetIcons}info.svg",
-                  semanticsLabel: 'Cycle Calendar Info',
-                ),
-              )
             ],
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          Text('How many times you urinated per day',
-              style: textTheme.bodySmall!
-                  .copyWith(color: AppColors.neutralColor500)),
-          const SizedBox(height: 24),
-          UrinationsChart(
-            data: widget.data,
           ),
         ],
       ),
@@ -75,19 +109,23 @@ class _InsightsTimeOfDayUrinationsChartState
   }
 }
 
-class UrinationsChart extends StatefulWidget {
+class ChartArea extends StatefulWidget {
   final InsightsGraphModel data;
 
-  const UrinationsChart({super.key, required this.data});
+  const ChartArea({super.key, required this.data});
 
   @override
-  State<UrinationsChart> createState() => _UrinationsChartState();
+  State<ChartArea> createState() => _ChartAreaState();
 }
 
-class _UrinationsChartState extends State<UrinationsChart> {
+class _ChartAreaState extends State<ChartArea> {
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
+    bool isDailyInterval = widget.data.isDailyInterval;
+    final allChartData = ChartHelper.getAllChartData(widget.data.graphData);
+    final firstDatesOfMonth = ChartHelper.getFirstDatesOfMonth(allChartData);
+
     return SizedBox(
       height: 25.h,
       child: SfCartesianChart(
@@ -100,53 +138,26 @@ class _UrinationsChartState extends State<UrinationsChart> {
             placeLabelsNearAxisLine: false,
             axisLine: const AxisLine(width: 0),
             majorTickLines: const MajorTickLines(width: 0),
-            maximumLabels: 5,
-            edgeLabelPlacement: EdgeLabelPlacement.shift,
+            interval: isDailyInterval ? 2 : 1,
+            axisLabelFormatter: (axisLabelRenderArgs) =>
+                ChartHelper.axisLabelFormatter(
+                    axisLabelRenderArgs, isDailyInterval, firstDatesOfMonth),
             majorGridLines: const MajorGridLines(width: 0),
             labelStyle: textTheme.labelSmall!
                 .copyWith(color: AppColors.neutralColor400, fontSize: 8)),
         primaryYAxis: NumericAxis(
           plotOffset: 0,
-          interval: getYAxisInterval(widget.data.getMaxValue()),
           axisLine: const AxisLine(width: 0),
-          minimum: 0,
           labelStyle: textTheme.labelSmall!.copyWith(
-              color: AppColors.neutralColor400,
-              fontSize: 8,
-              fontWeight: FontWeight.w500,
-              fontFamily: 'Poppins'),
-          axisLabelFormatter: (AxisLabelRenderDetails details) {
-            final value = details.value.toInt().toString();
-            return ChartAxisLabel(
-                value,
-                details.textStyle.copyWith(
-                    color: AppColors.neutralColor400,
-                    fontSize: 8,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'Poppins'));
-          },
+            color: AppColors.neutralColor400,
+            fontSize: 8,
+          ),
           majorGridLines:
-              const MajorGridLines(width: 1, color: AppColors.neutralColor200),
+          const MajorGridLines(width: 1, color: AppColors.neutralColor200),
           majorTickLines: const MajorTickLines(width: 0),
         ),
-        series: ChartHelper.getMaximumHeadacheBarSeries(widget.data),
+        series: ChartHelper.getHeadacheDurationBarSeries(widget.data),
       ),
     );
-  }
-}
-
-double getYAxisInterval(int maxValue) {
-  if (maxValue < 10) {
-    return 1;
-  } else if (maxValue <= 30) {
-    return 2;
-  } else if (maxValue <= 50) {
-    return 5;
-  } else if (maxValue <= 100) {
-    return 10;
-  } else if (maxValue <= 200) {
-    return 20;
-  } else {
-    return 50;
   }
 }
